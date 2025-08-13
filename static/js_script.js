7b2cb2a646a022083fc454c724c73fa27a1d7641
Tmazst <55834770+Tmazst@users.noreply.github.com>
2025-08-13 13:55:04 +0200

    recruitment piggs peak

--- static/js_script.js
+++ static/js_script.js
@@ -48,32 +48,61 @@
 //window.onscroll = function() {handleScroll()};
 
 // Scripts fades in the Services Cards Elements when they are in the viewport 
-function isInViewport(element) {
+// function isInViewport(element) {
+//     const rect = element.getBoundingClientRect();
+//     return (
+//         rect.top >= 0 &&
+//         rect.left >= 0 &&
+//         rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+//         rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+//     );
+// }
+
+// document.addEventListener('scroll', () => {
+//     const cards = document.querySelectorAll('.ser-card');
+//     const contactSection = document.querySelectorAll('.fader');
+//     cards.forEach((element, index) => {
+//         if (isInViewport(element)) {
+//             setTimeout(() => {
+//                 element.classList.add('fade-in');
+//             }, 350 * index);     
+//         } 
+//     });
+//     contactSection.forEach((element, index) => {
+//         if (isInViewport(element)) {
+//             setTimeout(() => {
+//                 element.classList.add('fade-in');
+//             }, 350 * index);     
+//         } 
+//     });
+// });
+
+function isInViewport(element, offset = 100) {
     const rect = element.getBoundingClientRect();
     return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        rect.top < (window.innerHeight - offset) &&
+        rect.bottom > 0
     );
 }
 
 document.addEventListener('scroll', () => {
     const cards = document.querySelectorAll('.ser-card');
     const contactSection = document.querySelectorAll('.fader');
+
     cards.forEach((element, index) => {
-        if (isInViewport(element)) {
+        if (isInViewport(element, 100)) {
             setTimeout(() => {
                 element.classList.add('fade-in');
-            }, 350 * index);     
-        } 
+            }, 350 * index);
+        }
     });
+
     contactSection.forEach((element, index) => {
-        if (isInViewport(element)) {
+        if (isInViewport(element, 100)) {
             setTimeout(() => {
                 element.classList.add('fade-in');
-            }, 350 * index);     
-        } 
+            }, 350 * index);
+        }
     });
 });
 
